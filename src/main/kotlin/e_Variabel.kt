1e2419b9ed1dbd5ea61030011b5cd449bca6f1e2
Sulaiman <mamanjr04@gmail.com>
2022-03-10 15:00:24 +0700

    Ini penerapan variabel pada Kotlin

---
+++ src/main/kotlin/e_Variabel.kt
@@ -0,0 +1,10 @@
+//Variabel adalah sebuah tempat/wadah didalam memori komputer untuk menyimpan data seperti data nama pengguna, kata sandi, data waktu, jumlah makanan dan lain-lain sebagainya
+
+//Contoh kode program:
+
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = $dynamicVar")
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = $staticVar")
+}
