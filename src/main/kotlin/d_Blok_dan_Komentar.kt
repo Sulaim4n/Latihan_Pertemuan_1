f6cfa3f8fd41cdc8e535992e6e471144eeecf5b0
Sulaiman <mamanjr04@gmail.com>
2022-03-10 14:56:11 +0700

    Ini penerapan Blok dan Komentar

---
+++ src/main/kotlin/d_Blok_dan_Komentar.kt
@@ -0,0 +1,13 @@
+//Blok digunakan untuk menulis ikatan sebuah statement yang dikelompokkan bersama menjadi satu bagian
+//Blok ditandai dengan tanda buka dan tutup kurung kurawal
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+    */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
+//Comment adalah baris komentar pada program yang tidak akan dieksekusi oleh compiler
